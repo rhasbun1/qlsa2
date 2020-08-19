4055080e237a4da5d46faa404c16039e97f25e76
Matias Canales <mcanales@spsgroup.cl>
2020-08-19 12:35:28 -0400

    Btn atras nota de venta, perfil cliente

--- sistema/resources/views/vernotaventa.blade.php
+++ sistema/resources/views/vernotaventa.blade.php
@@ -282,8 +282,12 @@
                         Session::get('idPerfil')=='3' or 
                         Session::get('idPerfil')=='4') )
                     <button class="btn btn-sm btn-danger" onclick="cerrarNotaVenta();">Pasar a Histórico</button> 
-                    @if ($accion == 1)                   
-                        <a href="{{ asset('/') }}listarNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                    @if ($accion == 1)
+                        @if ( Session::get('idPerfil') == '14' || Session::get('idPerfil') == '15')                   
+                            <a href="{{ asset('/') }}clienteNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                        @else
+                            <a href="{{ asset('/') }}listarNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                        @endif
                     @elseif ($accion == 2)
                         <a href="{{ asset('/') }}verpedido/{{ $item->idPedido }}/1-2/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
                     @elseif ($accion == 4)
@@ -293,7 +297,11 @@
                     @endif
                 @else
                     @if ($accion == 1)
-                        <a href="{{ asset('/') }}listarNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                        @if ( Session::get('idPerfil') == '14' || Session::get('idPerfil') == '15')                   
+                            <a href="{{ asset('/') }}clienteNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                        @else
+                            <a href="{{ asset('/') }}listarNotasdeVenta/" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>
+                        @endif
                     @elseif ($accion == 2)
                         <a href="{{ URL::previous() }}" class="btn btn-sm btn-warning" style="width:80px">Atrás</a>  
                     @elseif ($accion == 3)
